a0bb21ad8ef3b1accd25ceb39bbfdfd8fd9a4bf1
wenkrang <2934356978@qq.com>
2026-01-11 11:27:42 +0800

    Initial commit

---
+++ src/main/java/com/wenkrang/faUtilities/FaUtilities.java
@@ -0,0 +1,17 @@
+package com.wenkrang.faUtilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FaUtilities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
